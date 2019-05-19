1a786e2f59de24c38d3662406a49880a3cb09768
charlesmartinreed <charlesmartinreed@icloud.com>
2019-05-18 23:23:02 -0500

    Add single launch data field to RootQuery in schema. Add queries for Rockets and single Rocket to RootQuery.

--- schema.js
+++ schema.js
@@ -47,6 +47,37 @@ const RootQuery = new GraphQLObjectType({
           .get("https://api.spacexdata.com/v3/launches")
           .then(res => res.data);
       }
+    },
+    launch: {
+      type: LaunchType,
+      // args is what we use to query our results for a specific launch
+      args: {
+        flight_number: { type: GraphQLInt }
+      },
+      resolve(parentValue, args) {
+        return axios
+          .get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+          .then(res => res.data);
+      }
+    },
+    rockets: {
+      type: new GraphQLList(RocketType),
+      resolve(parentValue, args) {
+        return axios
+          .get("https://api.spacexdata.com/v3/rockets")
+          .then(res => res.data);
+      }
+    },
+    rocket: {
+      type: RocketType,
+      args: {
+        rocket_id: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        return axios(
+          `https://api.spacexdata.com/v3/rockets/${args.rocket_id}`
+        ).then(res => res.data);
+      }
     }
   }
 });
